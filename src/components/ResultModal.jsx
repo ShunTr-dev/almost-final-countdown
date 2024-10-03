ee7227f5fab24ee7a8edcd10c3c77e096512b156
ShunTr <repaco9@gmail.com>
2024-10-03 23:11:48 +0200

    Added portals

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 // The forwardRef function is a higher-order component that allows you to pass a ref to a child component.
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
@@ -14,7 +15,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
         },
     }));
 
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You Lost</h2>}
             {!userLost && <h2>Your Score: {score}</h2>}
@@ -31,7 +32,8 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     );
 });
 
